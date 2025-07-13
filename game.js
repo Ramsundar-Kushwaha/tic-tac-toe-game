9a0bbaefc2223b3f665fc3f21c8c345d4cbd3270
Ramsundar Kushwaha <ramsundarkushwaha31@gmail.com>
2025-07-13 17:03:50 +0545

    updated look

--- game.js
+++ game.js
@@ -1,16 +1,18 @@
 'use strict'
-let player = 1;
-let status = 0;
-let player1,player2;
+let firstPlayer = 1;
+let count = 0;
+let gameOver = false;
+let playerOneName, playerTwoName;
+
 
 window.addEventListener("DOMContentLoaded",function (){
   const PLAYER1 = localStorage.getItem("player1");
   const PLAYER2 = localStorage.getItem("player2");
   if(PLAYER1 && PLAYER2){
     document.getElementById("fp1").textContent=PLAYER1;
-    player1 = PLAYER1;
+    playerOneName = PLAYER1;
     document.getElementById("fp2").textContent=PLAYER2;
-    player2 = PLAYER2;
+    playerTwoName = PLAYER2;
   }else{
     alert("player names not found. Redirecting...");
     window.location.href="index.html";
@@ -18,120 +20,61 @@ window.addEventListener("DOMContentLoaded",function (){
 });
 
 for(let i = 0; i < 9; i++){
-  console.log("working");
   let btn = document.getElementById(`btn${i+1}`);
+
   btn.addEventListener("click", function(){
+    if(gameOver)return;
     if(btn.textContent == ""){
-      btn.textContent = (player == 1)?'🐘':'🐎';
-      status++;
+      btn.textContent = (firstPlayer == 1)?'❤️':'💕';
+      count++;
     }
-    player = (player == 1)?2:1;
-    Win_Check();
+    firstPlayer = (firstPlayer == 1)?2:1;
+    if(Win_Check()){
+      gameOver = true;
+    };
   });
 }
 
 //winCheck
 function Win_Check(){
-  if(status >= 5 && status <= 9){
-    //for horizontal cheaking
-    if(document.getElementById("btn1").textContent == document.getElementById("btn2").textContent && document.getElementById("btn2").textContent == document.getElementById("btn3").textContent && document.getElementById("btn3").textContent != ""){
+  let winnerFound = false;
+  function comboCheck(a, b, c){
+    const A = document.getElementById(a).textContent;
+    const B = document.getElementById(b).textContent;
+    const C = document.getElementById(c).textContent;
 
-      document.getElementById("btn1").style.backgroundColor = "#366566";
-      document.getElementById("btn2").style.backgroundColor = "#366566";
-      document.getElementById("btn3").style.backgroundColor = "#366566";
+    if(A == B && B == C && A != ""){
+      document.getElementById(a).style.backgroundColor = "green";
+      document.getElementById(b).style.backgroundColor = "green";
+      document.getElementById(c).style.backgroundColor = "green";
 
-      let x = (document.getElementById("btn1").textContent == '🐘')?`${player1} - Winner`:`${player2} - Winner`;
-
+      let x = (document.getElementById(a).textContent == '❤️')?`${playerOneName} - Winner`:`${playerTwoName} - Winner`;
       document.getElementById("win").textContent = x;
-      return;
+      winnerFound = true;
+      return true;
     }
-    else if(document.getElementById("btn4").textContent == document.getElementById("btn5").textContent && document.getElementById("btn5").textContent == document.getElementById("btn6").textContent && document.getElementById("btn6").textContent != ""){
+    return false;
+  }
 
-      document.getElementById("btn4").style.backgroundColor = "#366566";
-      document.getElementById("btn5").style.backgroundColor = "#366566";
-      document.getElementById("btn6").style.backgroundColor = "#366566";
+  //for horizontal check
+  if(comboCheck("btn1", "btn2", "btn3"))  return true;
+  if(comboCheck("btn4", "btn5", "btn6"))  return true;
+  if(comboCheck("btn7", "btn8", "btn9"))  return true;
 
-      alert((document.getElementById("btn4").textContent == '🐘')?`${player1}. - Winner`:`${player2} - Winner`);
-      return;
+  //for vertical check
+  if(comboCheck("btn1", "btn4", "btn7"))  return true;
+  if(comboCheck("btn2", "btn5", "btn8"))  return true;
+  if(comboCheck("btn3", "btn6", "btn9"))  return true;
+
+  //for diagonal check
+  if(comboCheck("btn1", "btn5", "btn9"))  return true;
+  if(comboCheck("btn3", "btn5", "btn7"))  return true;
+  
+  if(count == 9 && winnerFound == false){
+    for(let i = 0; i < 9; i++){
+      document.getElementById(`btn${i+1}`).style.backgroundColor = "yellow";
     }
-    else if(document.getElementById("btn7").textContent == document.getElementById("btn8").textContent && document.getElementById("btn8").textContent == document.getElementById("btn9").textContent && document.getElementById("btn9").textContent != ""){
-
-      document.getElementById("btn7").style.backgroundColor = "#366566";
-      document.getElementById("btn8").style.backgroundColor = "#366566";
-      document.getElementById("btn9").style.backgroundColor = "#366566";
-
-      let x = (document.getElementById("btn7").textContent == '🐘')?`${player1} - Winner`:`${player2} - Winner`;
-
-      document.getElementById("win").textContent = x;
-      return;
-    }
-
-    //for vertical cheaking
-    else if(document.getElementById("btn1").textContent == document.getElementById("btn4").textContent && document.getElementById("btn4").textContent == document.getElementById("btn7").textContent && document.getElementById("btn7").textContent != ""){
-
-      document.getElementById("btn1").style.backgroundColor = "#366566";
-      document.getElementById("btn4").style.backgroundColor = "#366566";
-      document.getElementById("btn7").style.backgroundColor = "#366566";
-
-      let x = (document.getElementById("btn1").textContent == '🐘')?`${player1} - Winner`:`${player2} - Winner`;
-
-      document.getElementById("win").textContent = x;
-      return;
-    }
-    else if(document.getElementById("btn2").textContent == document.getElementById("btn5").textContent && document.getElementById("btn5").textContent == document.getElementById("btn8").textContent && document.getElementById("btn8").textContent != ""){
-
-      document.getElementById("btn2").style.backgroundColor = "#366566";
-      document.getElementById("btn5").style.backgroundColor = "#366566";
-      document.getElementById("btn8").style.backgroundColor = "#366566";
-
-      let x = (document.getElementById("btn2").textContent == '🐘')?`${player1} - Winner`:`${player2} - Winner`;
-
-      document.getElementById("win").textContent = x;
-      return;
-    }
-    else if(document.getElementById("btn3").textContent == document.getElementById("btn6").textContent && document.getElementById("btn6").textContent == document.getElementById("btn9").textContent && document.getElementById("btn9").textContent != ""){
-
-      document.getElementById("btn3").style.backgroundColor = "#366566";
-      document.getElementById("btn6").style.backgroundColor = "#366566";
-      document.getElementById("btn9").style.backgroundColor = "#366566";
-
-      let x = (document.getElementById("btn3").textContent == '🐘')?`${player1} - Winner`:`${player2} - Winner`;
-
-      document.getElementById("win").textContent = x;
-      return;
-    }
-
-    //for diagonal cheaking
-    else if(document.getElementById("btn1").textContent == document.getElementById("btn5").textContent && document.getElementById("btn5").textContent == document.getElementById("btn9").textContent && document.getElementById("btn9").textContent != ""){
-      
-      document.getElementById("btn1").style.backgroundColor = "#366566";
-      document.getElementById("btn5").style.backgroundColor = "#366566";
-      document.getElementById("btn9").style.backgroundColor = "#366566";
-
-      let x = (document.getElementById("btn1").textContent == '🐘')?`${player1} - Winner`:`${player2} - Winner`;
-
-      document.getElementById("win").textContent = x;
-      return;
-    }
-    else if(document.getElementById("btn3").textContent == document.getElementById("btn5").textContent && document.getElementById("btn5").textContent == document.getElementById("btn7").textContent && document.getElementById("btn7").textContent != ""){
-      
-      document.getElementById("btn3").style.backgroundColor = "#366566";
-      document.getElementById("btn5").style.backgroundColor = "#366566";
-      document.getElementById("btn7").style.backgroundColor = "#366566";
-
-      let x = (document.getElementById("btn3").textContent == '🐘')?`${player1} - Winner`:`${player2} - Winner`;
-
-      document.getElementById("win").textContent = x;
-      return;
-    }
-
-    //if not any case match --> default case
-    else if(status == 9){
-      let x = "Match Draw";
-      for(let i = 0; i < 9; i++){
-        document.getElementById(`btn${i+1}`).style.backgroundColor = "red";
-      }
-      document.getElementById("win").textContent = x;
-    }
+    document.getElementById("win").textContent = "Match Draw";
+    return false;
   }
 }
--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 let submit = document.getElementById("submit");
 
 submit.addEventListener("click", function(){
-  const player1 = document.getElementById("playerOne").value;
-  const player2 = document.getElementById("playerTwo").value;
-  if(player1 && player2){
-    localStorage.setItem("player1",player1);
-    localStorage.setItem("player2",player2);
-    window.location.href = "game.html"; // redirect to game page
+  const PLAYER1 = document.getElementById("playerOne").value;
+  const PLAYER2 = document.getElementById("playerTwo").value;
+  if(PLAYER1 && PLAYER2){
+    localStorage.setItem("player1",PLAYER1);
+    localStorage.setItem("player2",PLAYER2);
+    window.location.href = "game.html";// redirect to game page
   }
   else{
     alert("please enter both player names.");
